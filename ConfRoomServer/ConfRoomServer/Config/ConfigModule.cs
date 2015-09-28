16938799a96b94b0abf86f525776cfdd6f145910
rburton5403 <rodney@innovatetank.com>
2015-09-28 11:25:59 -0500

    config module

---
+++ ConfRoomServer/ConfRoomServer/Config/ConfigModule.cs
@@ -0,0 +1,36 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfRoomServer.Config
+{
+    public class ConfigModule : NancyModule
+    {
+        public ConfigModule()
+            : base()
+        {
+            registerRoutes();
+        }
+
+        private void registerRoutes()
+        {
+            Get["/config"] = GetConfig();
+        }
+
+        private Func<dynamic, dynamic> GetConfig()
+        {
+            return p =>
+            {
+                var config = new {
+                    AvailableColor = "green",
+                    BusyColor = "red"
+                };
+                return Response.AsJson(config);
+            };
+        }
+
+    } //end of class
+}
